07c65f61d8c3aa9ac4b7b9cffd3d458311269241
luksfay <lukspicco@hotmail.com>
2021-09-16 09:55:40 -0300

    comunicacion resuelta, ya no tiene warnings. hacia doble funcion

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,19 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 const Title = ({setTitle}) => {
-
-    const [titleName, setTitleName] = useState('');
-
+    //En esta función le estoy diciendo al state de title del archivo app que a traves del valor del input represente la informacion recibida
     const defineTitleName = (e) => {
-        setTitleName(e.target.value);
+        setTitle(e.target.value);
     }
-    useEffect(() => {
-        return(
-            // Esto estaba separado no adentro de un useEffect, quise ponerlo aca adentro para tratar de resolver el warning de la consola.
-            setTitle(titleName)
-        )
-    }, [])
-    
     return(
         <div>
             <input type="text" name="title" onChange={defineTitleName} />
